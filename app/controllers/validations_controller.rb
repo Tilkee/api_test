aebde601e07bc58c155f8e02672020a7ca284697
putnich <putnich.m@gmail.com>
2019-02-03 21:18:13 +0000

    Added more tests

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -5,96 +5,103 @@ class ValidationsController < ApplicationController
   
   # POST /validations
   def validate_post
-    logger = Rails.logger
-    @validation = Validation.new(validation_params)
-    @types = Conditions.types()
-    @ops = Conditions.ops()
-    @conds = Conditions.conds()
+      logger = Rails.logger
+      @validation = Validation.new(validation_params)
+      @types = Conditions.types()
+      @ops = Conditions.ops()
+      @conds = Conditions.conds()
     
-    e = @validation.get_exp();
-    s = e.split();
+      expr = @validation.get_exp();
+      s = expr.split(" ");
+      e = []
+      bracket = 0
+      valid = true
+      
+      s.each do |i|
+        if i == '('
+          bracket-=1
+          next
+        end
+        if i == ')'
+          bracket+=1
+          next
+        end
+        if i == "&&" || i == "||"
+          valid = evaluate(e)
+          e = []
+          next
+        end
+        e << i
+      end
+      valid = evaluate(e)
+      if bracket != 0
+        valid = false
+        logger.error("  Error: Missing brackets")
+      end
     
-    i = 0; len = s.length; bracket = 0
-    valid = true
-    while i <= len && valid
-      if i == len
-        valid = evaluate(e)
-        break
+      if valid
+        render :status => :ok
+      else
+        render :status => :bad_request
       end
-      if s[i] == "("
-        bracket-=1
-        i+=1
-        next
-      end
-      if s[i] == ")"
-        bracket+=1
-        i+=1
-        next
-      end
-      if s[i] == "&&" || s[i] == "||"
-        valid = evaluate(e)
-        e = nil
-        i+=1
-        next
-      end
-      e = s[i..i+3]
-      i+=3
-    end
-    if bracket != 0
-      valid = false
-      logger.error("  Error: Missing brackets")
-    end
-    if valid
-      render :status => :ok
-    else
-      render :status => :bad_request
-    end
+      
   end
   
   def evaluate(e)
-    valid = false
-    c = e[0].split(".")
-    begin
-      type = @conds[c[0].to_sym][:fields][c[1].to_sym][:type]
-    rescue
-      logger.error "  Error: Unidentified object: #{e[0]}"
-      return valid
-    end
+      c = e[0].split(".")
+      begin
+        type = @conds[c[0].to_sym][:fields][c[1].to_sym][:type]
+      rescue
+        logger.error "  Error: Unidentified object: #{e[0]}"
+        return false
+      end
     
-    op = e[1]
-    val = e[2]
+      op = e[1]
+      val = e[2]
     
-    @ops.each do |k,v|
-      if v[:operator] == op ||  v[:front_exp]
-        valid = true
-        break
+      is_op_correct = false
+      @ops.each do |k,v|
+        if v[:operator] == op ||  v[:front_exp] == op
+          is_op_correct = true
+          break
+        end
       end
-    end
     
-    case type
-    when "number"
-      valid = (/^[+-]?([0-9]+[.])?[0-9]+$/ === val)
-      if !valid
-        logger.error "  Error: value type is not a NUMBER"
+      if !is_op_correct 
+        logger.error "  Error: operator is not correct"
+        return false
       end
-    when "boolean"
-      valid = (val == "false" || val == "true")
-      if !valid 
-        logger.error "  Error: value type is not a BOOLEAN"
+    
+      is_type_correct = true
+      case type
+      when "number"
+        is_type_correct = (/^[+-]?([0-9]+[.])?[0-9]+$/ === val)
+        if !is_type_correct
+          logger.error "  Error: value type is not a NUMBER"
+        end
+      when "boolean"
+        is_type_correct = (val == "false" || val == "true")
+        if !is_type_correct 
+          logger.error "  Error: value type is not a BOOLEAN"
+        end
+      when "string"
+        is_type_correct = (val.start_with?("'") && val.end_with?("'"))
+        if !is_type_correct 
+          logger.error "  Error: value type is not a STRING"
+        end
       end
-    when "string"
-      valid = (val.start_with?("'") && val.end_with?("'"))
-      if !valid 
-        logger.error "  Error: value type is not a STRING"
-      end
-    end
         
-    return valid
+      if !is_type_correct
+        return false
+      else
+        return true
+      end
+    
   end
   
   # Only allow a trusted parameter "white list" through.
   def validation_params
-    params.require(:validation)
+      params.require(:validation)
   end
-  
+
 end
--- spec/controllers/validations_controller_spec.rb
+++ spec/controllers/validations_controller_spec.rb
@@ -6,6 +6,7 @@ logger = Rails.logger
     context "validate post request" do
       
       it "detects if 200 ok" do
+        
         logger.info "Test 1"
         post :validate_post, params: {validation: "connexion.percentage_read > 50"}
         expect(response.code).to eq "200"
@@ -13,25 +14,50 @@ logger = Rails.logger
         logger.info "Test 2"
         post :validate_post, params: {validation: "connexion.percentage_read > 50 && ( connexion.total_time > 2000 && connexion.total_time < 2020202020 )"}
         expect(response.code).to eq "200"
+        
+        logger.info "Test 3"
+        post :validate_post, params: {validation: "connexion.percentage_read > 50 || connexion.total_time > 2000"}
+        expect(response.code).to eq "200" 
+      
+        
+        logger.info "Test 4"
+        post :validate_post, params: {validation: "( connexion.percentage_read > 50 ) && ( connexion.total_time >= 2000 )"}
+        expect(response.code).to eq "200"
+        
       end
       
       it "detects if 400 bad request" do
-        logger.info "Test 3"
+        logger.info "Test 5"
         post :validate_post, params: {validation: "connexion.percentage_red > 50"}
         expect(response.code).to eq "400"
         
-        logger.info "Test 4"
+        logger.info "Test 6"
         post :validate_post, params: {validation: "connexion.percentage_read > 'bonjour'"}
         expect(response.code).to eq "400"
         
-        logger.info "Test 5"
+        logger.info "Test 7"
         post :validate_post, params: {validation: "token.name =~ 12"}
         expect(response.code).to eq "400"
         
-        logger.info "Test 6"
+        logger.info "Test 8"
         post :validate_post, params: {validation: "undefined_object.name =~ 'bonjour'"}
         expect(response.code).to eq "400"
         
+        logger.info "Test 9"
+        post :validate_post, params: {validation: "( connexion.percentage_read > 50"}
+        expect(response.code).to eq "400"
+        
+        logger.info "Test 10"
+        post :validate_post, params: {validation: "connexion.percentage_read >>> 50"}
+        expect(response.code).to eq "400"
+        
+        logger.info "Test 11"
+        post :validate_post, params: {validation: "connexion.percentage_read < 5ffeffe0"}
+        expect(response.code).to eq "400"
+        
+        logger.info "Test 12"
+        post :validate_post, params: {validation: "123456789 45678 567890"}
+        expect(response.code).to eq "400"
         
       end
     end
